56d13465fd20f5880ce79def9e0e19d69b4fbf0d
roger-roger-svg <rogercunhadeandrade@gmail.com>
2023-12-30 22:18:09 -0300

    `home screen` 30/12

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,208 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  Pressable,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { MaterialCommmunityIcons } from "@expo/vector-icons";
 
 const HomeScreen = () => {
-  <View>
-    <Text>HomeScreen</Text>
-  </View>;
+  const [userProfile, setUserProfile] = useState([]);
+  const [recentlyplayed, setRecentlyPlayed] = useState([]);
+  const greetingMessage = () => {
+    const currentTime = new Date().getHours();
+    if (currentTime < 12) {
+      return "Bom dia";
+    } else if (currentTime < 16) {
+      return "Boa tarde";
+    } else {
+      return "Boa noite";
+    }
+  };
+
+  const message = greetingMessage();
+  const getProfile = async () => {
+    const accessToken = await AsyncStorage.getItem("token");
+    try {
+      const response = await fetch("https://api.spotify.com/v1/me", {
+        headers: {
+          Authorization: `Bearer + ${accessToken}`,
+        },
+      });
+      const data = await response.json();
+      setUserProfile(data);
+      return data;
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  useEffect(() => {
+    getProfile();
+    console.log(er.message);
+  }, []);
+  console.log(userProfile);
+  const getRecentlyPlayedSongs = async () => {
+    const accessToken = await AsyncStorage.getItem("token");
+    try {
+      const response = await axios({
+        method: "GET",
+        url: "https://api.spotify.com/v1/me/player/recently-played?limit=4",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      const tracks = response.data.items;
+      setRecentlyPlayed(tracks);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  useEffect(() => {
+    getRecentlyPlayedSongs();
+  }, []);
+  console.log(recentlyplayed);
+  return (
+    <LinearGradient colors={["#040306", "#131624"]} style={{ flex: 1 }}>
+      <ScrollView style={{ marginTop: 50 }}>
+        <View
+          style={{
+            padding: 10,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Image
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: 20,
+                resizeMode: "cover",
+              }}
+              source={{ uri: userProfile?.images[0].uri }}
+            />
+            <Text
+              style={{
+                marginLeft: 10,
+                fontSize: 20,
+                fontWeight: "bold",
+                color: "white",
+              }}
+            >
+              {message}
+            </Text>
+          </View>
+          <MaterialCommmunityIcons
+            name="lightning-bolt-outline"
+            size={24}
+            color="white"
+          />
+        </View>
+        <View
+          style={{
+            marginHorizontal: 12,
+            marginVertical: 5,
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 10,
+          }}
+        >
+          <Pressable
+            style={{
+              backgroundColor: "#282828",
+              padding: 10,
+              borderRadius: 30,
+            }}
+          >
+            <Text style={{ fontSize: 15, color: "white" }}>Música</Text>
+          </Pressable>
+          <Pressable
+            style={{
+              backgroundColor: "#282828",
+              padding: 10,
+              borderRadius: 30,
+            }}
+          >
+            <Text style={{ fontSize: 15, color: "white" }}>
+              Podcasts & Shows
+            </Text>
+          </Pressable>
+        </View>
+
+        <View style={{ height: 10 }} />
+
+        <View>
+          <Pressable
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 10,
+              flex: 1,
+              flexDirection: "row",
+              marginVertical: 8,
+              backgroundColor: "#202020",
+              borderRadius: 4,
+              elevation: 3,
+            }}
+          >
+            <LinearGradient colors={["#33006f", "#FFFFFF"]}>
+              <Pressable
+                style={{
+                  width: 55,
+                  height: 55,
+                  justifyContent: "center",
+                  alignContent: "center",
+                }}
+              >
+                <AntDesign name="heart" size={24} color="black" />
+              </Pressable>
+            </LinearGradient>
+            <Text style={{ color: "white", fontSize: 10, fontWeight: "bold" }}>
+              Sons curtidos
+            </Text>
+          </Pressable>
+        </View>
+        <View style={{ height: 10 }} />
+
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-around",
+          }}
+        >
+          <Pressable></Pressable>
+
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 10,
+              flex: 1,
+              flexDirection: "row",
+              marginVertical: 8,
+              backgroundColor: "#202020",
+              borderRadius: 4,
+              elevation: 3,
+            }}
+          >
+            <Image
+              style={{ width: 55, height: 55 }}
+              source={{ uri: "https://i.pravatar.cc/100" }}
+            />
+            <View style={styles.randomArtist}></View>
+            <Text>Hiphop</Text>
+          </View>
+        </View>
+      </ScrollView>
+    </LinearGradient>
+  );
 };
 
 export default HomeScreen;
